39a395778a300ec2f9e1d048b242297a8847b359
Roberto <71038468+roberto-pano@users.noreply.github.com>
2025-05-21 21:16:40 -0700

    React Native

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './cosmic-pony/web/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
