7a0dd97b11db4990ec59d15e2f6d1122a460bf49
Roberto <p.roberto@wustl.edu>
2025-11-04 01:15:01 -0800

    vite and react native compatibiliy

---
+++ src/router/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+// Import screens/pages
+import Home from '../screens/Home';
+import About from '../screens/About';
+import Projects from '../screens/Projects';
+import Contact from '../screens/Contact';
+import MexicanVisualCultureProject from '../screens/MexicanVisualCultureProject';
+import Calendar from '../screens/Calendar';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="/mexican-visual-culture"
+          element={<MexicanVisualCultureProject />}
+        />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
